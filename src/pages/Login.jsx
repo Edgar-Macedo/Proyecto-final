7549cebdd29d98403d2eccafae992639ff23d694
Edgar-Macedo <edgar.ml0814@gmail.com>
2022-08-11 23:27:01 -0500

    Added placeholders

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -38,7 +38,7 @@ const Login = () => {
                 <Form onSubmit={handleSubmit(submit)}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email"{... register("email")} />
+                        <Form.Control type="email" placeholder="max@gmail.com"{... register("email")} />
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                         </Form.Text>
@@ -46,7 +46,7 @@ const Login = () => {
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password"{... register("password")} />
+                        <Form.Control type="password" placeholder="pass1234"{... register("password")} />
                     </Form.Group>
                     
                     <Button variant="primary" type="submit">
